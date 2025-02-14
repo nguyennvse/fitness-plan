4bc6821f411739423d99c2bf08d898470cebf8e5
Nguyen Nguyen <nguyen.nv@samsung.com>
2025-02-14 17:09:55 +0700

    init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatIconModule,RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'your-fitness-plan';
+  navigationMenus = [
+    {name:'Home',url:'/home'},
+    {name:'Foods',url:'/foods'},
+    {name:'Classes',url:'/classes'},
+    {name:'Equipments',url:'/equipments'},
+    {name:'Our Team',url:'/ourteam'},
+  ].map((menu, index) => ({ index, ...menu }));
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home/home-component/home-component.component';
+import { ClassesComponent } from './classes/classes.component';
+import { EquipmentsComponent } from './equipments/equipments.component';
+import { FoodsComponent } from './foods/foods.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomePageComponent},
+    {path: 'foods', component: FoodsComponent},
+    {path: 'classes', component: ClassesComponent},
+    {path: 'equipments', component: EquipmentsComponent}
+];
